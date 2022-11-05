c75ca42320110a236d03baec4dea7f5855bba29e
Jeanette <hiddensquiddy@gmail.com>
2022-11-04 17:49:08 -0700

    numbers

--- GenerateTime.py
+++ GenerateTime.py
@@ -1,16 +1,16 @@
 class GenerateTimeString():
     def __init__(self) -> None:
         self.numbers = {
-            0: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
+            0: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [False, False, False, False], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
             1: [[False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
-            2: [[], [], [], [], [], [], []],
-            3: [[], [], [], [], [], [], []],
-            4: [[], [], [], [], [], [], []],
-            5: [[], [], [], [], [], [], []],
-            6: [[], [], [], [], [], [], []],
-            7: [[], [], [], [], [], [], []],
-            8: [[], [], [], [], [], [], []],
-            9: [[], [], [], [], [], [], []]
+            2: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True]],
+            3: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]],
+            4: [[True, False, False, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
+            5: [[True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]],
+            6: [[True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
+            7: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
+            8: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
+            9: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]]
         }
 
     def parse_time_object():
