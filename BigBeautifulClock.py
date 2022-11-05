0f9268766bb9f0e6b254e8a5312db890cfa93699
Dissonant <kak100kak@gmail.com>
2022-11-04 20:21:15 -0400

    Update BigBeautifulClock.py

--- BigBeautifulClock.py
+++ BigBeautifulClock.py
@@ -1,14 +1,21 @@
+import discord
+from discord.ext import commands
 import interactions
 import os
 from dotenv import load_dotenv
 
-class CustomBotClient(interactions.Client):
+
+class CustomBotClient(commands.Bot):
     def __init__(self):
-        super().__init__()
+        super().__init__(command_prefix="$", intents=discord.Intents.all())
+    
+    async def on_ready(self):
+        print(f"{self.user.name} is ready to rumble!")
+
 
 if __name__ == "__main__":
     load_dotenv()
     DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-    bot = interactions.Client(token=DISCORD_TOKEN)
     
-    bot.start()
+    bot = CustomBotClient()
+    bot.run(DISCORD_TOKEN)
