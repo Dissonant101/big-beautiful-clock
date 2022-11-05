126b1ee9024b5aeb147cb391c132e7643ece0d64
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-11-04 20:31:37 -0400

    Added matrices for 0, 1

--- GenerateTime.py
+++ GenerateTime.py
@@ -1,20 +1,20 @@
 class GenerateTimeString():
     def __init__(self) -> None:
         self.numbers = {
-            0: [],
-            1: [],
-            2: [],
-            3: [],
-            4: [],
-            5: [],
-            6: [],
-            7: [],
-            8: [],
-            9: []
+            0: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
+            1: [[False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
+            2: [[], [], [], [], [], [], []],
+            3: [[], [], [], [], [], [], []],
+            4: [[], [], [], [], [], [], []],
+            5: [[], [], [], [], [], [], []],
+            6: [[], [], [], [], [], [], []],
+            7: [[], [], [], [], [], [], []],
+            8: [[], [], [], [], [], [], []],
+            9: [[], [], [], [], [], [], []]
         }
 
-    def parseTimeObject():
+    def parse_time_object():
         return
 
-    def generateString():
+    def generate_string():
         return
