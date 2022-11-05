9706cabe0acd79b5d7d2e96cabce2ccc9457d739
Dissonant <kak100kak@gmail.com>
2022-11-04 22:55:08 -0400

    Save message IDs of clocks

--- cogs/create_clock.py
+++ cogs/create_clock.py
@@ -6,16 +6,18 @@ from helpers.generate_time import GenerateTimeString
 
 
 class CreateClock(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.generator = GenerateTimeString()
 
     @app_commands.command(name="create_clock", description="Creates a clock!")
-    async def create_clock(self, interaction):
+    async def create_clock(self, interaction: discord.Interaction):
         await interaction.response.defer()
         message = await interaction.followup.send(f"Creating clock!")
         parsed_time = self.generator.parse_time_object(dt.datetime.now())
         await message.edit(content=self.generator.generate_string(parsed_time))
+        await self.bot.get_channel(self.bot.instances_channel).send(message.id)
+        self.bot.instances.append(message.id)
 
 
 async def setup(bot):
