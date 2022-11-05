79bd30c04162246baecdefefa930bde00b32831f
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-11-04 20:22:17 -0400

    Create GenerateTime.py

---
+++ GenerateTime.py
@@ -0,0 +1,20 @@
+class GenerateTimeString():
+    def __init__(self) -> None:
+        self.numbers = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: []
+        }
+
+    def parseTimeObject():
+        return
+
+    def generateString():
+        return
