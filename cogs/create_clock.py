1cf0617c8421692cf0b8d1ad56cdc426fd45e904
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-11-04 22:26:09 -0400

    Reduced character count for all numbers

--- cogs/create_clock.py
+++ cogs/create_clock.py
@@ -4,17 +4,18 @@ from discord.ext import commands
 import datetime as dt
 from helpers.generate_time import GenerateTimeString
 
+
 class CreateClock(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.generator = GenerateTimeString()
-    
+
     @app_commands.command(name="create_clock", description="Creates a clock!")
     async def create_clock(self, interaction):
         await interaction.response.defer()
         message = await interaction.followup.send(f"Creating clock!")
         parsed_time = self.generator.parse_time_object(dt.datetime.now())
-        await message.edit(content=self.generator.generate_string(parsed_time, True))
+        await message.edit(content=self.generator.generate_string(parsed_time))
 
 
 async def setup(bot):
--- helpers/generate_time.py
+++ helpers/generate_time.py
@@ -4,16 +4,16 @@ from datetime import datetime, timezone
 class GenerateTimeString():
     def __init__(self) -> None:
         self.numbers = {
-            0: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, False, False, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
-            1: [[False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
-            2: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True]],
-            3: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]],
-            4: [[True, False, False, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
-            5: [[True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]],
-            6: [[True, True, True, True], [True, False, False, False], [True, False, False, False], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
-            7: [[True, True, True, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True], [False, False, False, True]],
-            8: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True]],
-            9: [[True, True, True, True], [True, False, False, True], [True, False, False, True], [True, True, True, True], [False, False, False, True], [False, False, False, True], [True, True, True, True]]
+            0: [[True, True, True], [True, False, True], [True, False, True], [True, False, True], [True, False, True], [True, False, True], [True, True, True]],
+            1: [[False, True], [False, True], [False, True], [False, True], [False, True], [False, True], [False, True]],
+            2: [[True, True, True], [False, False, True], [False, False, True], [True, True, True], [True, False, False], [True, False, False], [True, True, True]],
+            3: [[True, True, True], [False, False, True], [False, False, True], [True, True, True], [False, False, True], [False, False, True], [True, True, True]],
+            4: [[True, False, True], [True, False, True], [True, False, True], [True, True, True], [False, False, True], [False, False, True], [False, False, True]],
+            5: [[True, True, True], [True, False, False], [True, False, False], [True, True, True], [False, False, True], [False, False, True], [True, True, True]],
+            6: [[True, True, True], [True, False, False], [True, False, False], [True, True, True], [True, False, True], [True, False, True], [True, True, True]],
+            7: [[True, True, True], [False, False, True], [False, False, True], [False, False, True], [False, False, True], [False, False, True], [False, False, True]],
+            8: [[True, True, True], [True, False, True], [True, False, True], [True, True, True], [True, False, True], [True, False, True], [True, True, True]],
+            9: [[True, True, True], [True, False, True], [True, False, True], [True, True, True], [False, False, True], [False, False, True], [True, True, True]]
         }
 
     def parse_time_object(self, dt: datetime) -> tuple:
