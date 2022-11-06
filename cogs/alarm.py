8650d59a12a8897ab81cb2e440592321494c85a6
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-11-06 02:15:03 -0500

    added days to alarms

--- cogs/alarm.py
+++ cogs/alarm.py
@@ -52,16 +52,28 @@ class Alarms(commands.Cog):
         day = int(day)
         hour = int(hour)
         minute = int(minute)
-        message = await interaction.followup.send(f"Creating alarm!")
+        message1 = await interaction.followup.send(f"Creating alarm!")
         end_date_time = datetime(datetime.now().year, month, day, hour, minute)
         end_time = end_date_time.replace(
             microsecond=0).strftime("%m/%d, %H:%M")
         time_difference = self.get_time_difference(
             end_date_time, datetime.now())
-        await message.edit(content=(f"Alarm set for: {end_time}, {tzs[timezone]}\n" + self.generator.generate_string(time_difference[1])))
-        await self.bot.get_channel(self.bot.instances_channel).send(f"s {interaction.channel_id} {message.id} {end_time} {timezone} {description}")
+        msg_1_content = f"Alarm set for: {end_time}, {tzs[timezone]}\n"
+        msg_2_content = ""
+        message2 = None
+        if time_difference[0] > 0:
+            msg_1_content += self.generator.generate_day_string(
+                time_difference[0])
+            msg_2_content = self.generator.generate_string(time_difference[1])
+        else:
+            msg_1_content += self.generator.generate_string(time_difference[1])
+        await message1.edit(content=(msg_1_content))
+        if time_difference[0] > 0:
+            message2 = await interaction.followup.send(msg_2_content)
+        # self.generator.generate_string(time_difference[1])
+        await self.bot.get_channel(self.bot.instances_channel).send(f"s {interaction.channel_id} {message1.id} {end_time} {timezone} {description}")
         self.bot.instances["alarms"].append(
-            [interaction.channel_id, message.id, end_time, timezone, description])
+            [interaction.channel_id, message1.id, end_time, timezone, description, message2])
 
     @staticmethod
     def check_time_difference(t1: datetime, t2: datetime) -> bool:
@@ -84,7 +96,6 @@ class Alarms(commands.Cog):
         minute_difference = t1.minute - t2.minute
         second_difference = t1.second - t2.second
         day_difference = t1.day - t2.day
-        month_difference = t1.month - t2.month
         if second_difference < 0:
             minute_difference -= 1
             second_difference += 60
@@ -94,36 +105,56 @@ class Alarms(commands.Cog):
         if hour_difference < 0:
             day_difference -= 1
             hour_difference += 24
-        return (month_difference, day_difference), (hour_difference, minute_difference, second_difference)
+        return day_difference, (hour_difference, minute_difference, second_difference)
 
     @tasks.loop(seconds=1)  # checks if alarm time is reaching actual time
     async def check_times(self):
         self.count += 1
         alarms_to_be_deleted = []
         for i in range(len(self.bot.instances["alarms"])):
-            channel_id, message_id, end_time, tz, description = self.bot.instances["alarms"][i]
+            channel_id, message1_id, end_time, tz, description, message2_id = self.bot.instances[
+                "alarms"][i]
             timezone = pytz.timezone(tz)
             current_time = datetime.now(timezone)
             alarm_time = datetime.strptime(
                 end_time, "%m/%d, %H:%M").replace(year=current_time.year, tzinfo=timezone)
             if self.check_time_difference(alarm_time, current_time):
-                message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-                await message.edit(content=message.content[:32] + "\n" + self.generator.generate_string((0, 0, 0)))
+                message1 = await self.bot.get_channel(channel_id).fetch_message(message1_id)
+                await message1.edit(content=message1.content[:32] + "\n" + self.generator.generate_string((0, 0, 0)))
                 await self.bot.get_channel(channel_id).send(f"@everyone **{description}**")
                 alarms_to_be_deleted.append(i)
             else:
                 if self.count % 3 == 0:
-                    await self.count_down_alarm(channel_id, message_id, alarm_time, 32, timezone, current_time)
+                    got_rid_of_days = await self.count_down_alarm(channel_id, message1_id, alarm_time, 32, current_time, message2_id)
+                    if got_rid_of_days:
+                        self.bot.instances["alarms"][i] = (
+                            channel_id, message1, end_time, tz, description, None)
         for i in range(len(alarms_to_be_deleted)):
             for j in range(i, len(alarms_to_be_deleted)):
                 alarms_to_be_deleted[j] -= 1
             self.bot.instances["alarms"].pop(i)
 
-    async def count_down_alarm(self, channel_id: int, message_id: int, alarm_time: datetime, first_msg_len: int, timezone: pytz.timezone, current_time: datetime):
-        message = await self.bot.get_channel(channel_id).fetch_message(message_id)
-        first = message.content[:first_msg_len]
-        time_difference = self.get_time_difference(alarm_time, current_time)
-        await message.edit(content=first + "\n" + self.generator.generate_string(time_difference[1]))
+    async def count_down_alarm(self, channel_id: int, message1_id: int, alarm_time: datetime, first_msg_len: int, current_time: datetime, message2_id) -> bool:
+        if message2_id is None:
+            message = await self.bot.get_channel(channel_id).fetch_message(message1_id)
+            first = message.content[:first_msg_len]
+            time_difference = self.get_time_difference(
+                alarm_time, current_time)
+            await message.edit(content=first + "\n" + self.generator.generate_string(time_difference[1]))
+        else:
+            message1 = await self.bot.get_channel(channel_id).fetch_message(message1_id)
+            message2 = await self.bot.get_channel(channel_id).fetch_message(message2_id)
+            first = message.content[:first_msg_len]
+            time_difference = self.get_time_difference(
+                alarm_time, current_time)
+            if time_difference[0] > 0:
+                await message1.edit(content=first+"\n" + self.generator.generate_day_string(time_difference[0]))
+                await message2.edit(content=self.generator.generate_string(time_difference[1]))
+            else:
+                await message1.edit(content=first+"\n" + self.generator.generate_string(time_difference[1]))
+                await message2.delete()
+                return True
+        return False
 
     @check_times.before_loop
     async def before_check_times(self):
